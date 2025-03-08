0f2d03e436b8e578669577617dac333dd99859a8
spagwhetti <spgeti4@gmail.com>
2025-03-08 09:01:37 +0000

    Update extra.js

--- javascript/extra.js
+++ javascript/extra.js
@@ -11,27 +11,27 @@ BUNDLE_VER = "c302ed7ee42cd709e3cc0475d7cadf5b";
         } else if (clickhere.src.endsWith("clickhere_blue")) {
             clickhere.src = "/assets/img/clickhere_red.gif";
         }
-    }),
+    });
     duck.onmouseover = () => {
         new Audio("/assets/audio/quack.mp3").load();
-    },
+    };
     duck.onclick = () => {
         const a = new Audio("/assets/audio/quack.mp3");
         a.currentTime = 0.1;
         a.play();
-    },
+    };
     eyes.onclick = () => {
         let e = document.createElement("div");
         e.classList.add("blink"),
         document.body.append(e),
         setTimeout(() => e.remove(), 400);
-    },
+    };
     ygm.onmouseover = () => {
         new Audio("/assets/audio/youvegotmail.mp3").load();
-    },
+    };
     ygm.onclick = () => {
         const a = new Audio("/assets/audio/youvegotmail.mp3");
         a.currentTime = 0.1; 
         a.play();
-    },
+    };
 })();
