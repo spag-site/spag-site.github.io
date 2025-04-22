d23584dce5163877de3e6fd76fb40f5fa111411a
daisy <spgeti4@gmail.com>
2025-04-22 15:53:53 +0100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -40,6 +40,7 @@ fetch(galleryDataUrl)
             const image = document.createElement('img');
             image.src = `https://spag-site.github.io${file}`; // Set the image source
             image.alt = file; // set alt text
+            image.class = "mi";
             image.style.width = '320px'; // set image width
             image.style.margin = '4px';  // spacing between images
             imagesContainer.appendChild(image);
