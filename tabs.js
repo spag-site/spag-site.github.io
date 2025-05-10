393b3f78a019493a521c7dfd2688a77fe74e29b3
daisy <spgeti4@gmail.com>
2025-05-10 15:10:35 +0100

    Create tabs.js

---
+++ tabs.js
@@ -0,0 +1,35 @@
+document.getElementById("openTab").click();
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const gallery = urlParams.get("gallery");
+const webring = urlParams.get("webring");
+
+if (gallery) {
+  openTab(event, "gallery");
+}
+
+if (webring) {
+  openTab(event, "webring");
+}
